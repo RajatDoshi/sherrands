dda8cfbfe161a10121ec5445f3f1b6e17d95fe86
Rajat Doshi <rajatdoshi@hotmail.com>
2020-05-04 23:13:28 -0400

    improved database management for users

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#FLASK_APP=main.py FLASK_ENV=development flask run --port 4045
-
-from flask import Flask, render_template, url_for, request, redirect
+#FLASK_APP=main.py FLASK_ENV=development flask run --port 4047
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_BINDS'] = {'logins': 'sqlite:///logins.db'}
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -18,6 +18,16 @@ class Todo(db.Model):
 
 	def __repr__(self):
 		return '<Task %r>' % self.id
+
+class Logins(db.Model):
+	__bind_key__ = 'logins'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(200))
+	first_name = db.Column(db.String(200))
+	last_name = db.Column(db.String(200))
+	email = db.Column(db.String(200))
+	address = db.Column(db.String(200))
+	password = db.Column(db.String(200))
 
 @app.route('/', methods=['POST', 'GET']) 
 def index(): 
@@ -39,6 +49,29 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/process_signup', methods=['POST', 'GET']) 
+def process_signup():
+	if request.method == 'POST':
+		uname = request.form['username']
+		fname = request.form['first_name']
+		lname = request.form['last_name']
+		email_info = request.form['email']
+		addy = request.form['address']
+		password_info = request.form['password']
+		repeat_pass = request.form['psw-repeat']
+		if not password_info == repeat_pass:
+			return "passwords don't match"
+		login_var = Logins(username=uname, first_name=fname, last_name=lname, email=email_info, address=addy, password=password_info)
+		try:
+			db.session.add(login_var)
+			db.session.commit()
+		except Exception as e:
+			return str(e)
+		return redirect('/signin')
+	else:
+		return "GET REQUEST TO PROCESS SIGNUP"
+
+
 @app.route('/signup', methods=['POST', 'GET']) 
 def signup():
 	if request.method == 'POST':
@@ -46,10 +79,15 @@ def signup():
 	else:
 		return render_template('createAccount.html')
 
-@app.route('/signin.html', methods=['POST', 'GET']) 
+@app.route('/signin', methods=['POST', 'GET']) 
 def signin():
 	if request.method == 'POST':
-		return "POST COMMAND TO signIn"
+		uname = request.form['uname']
+		password_info = request.form['psw']
+		related_password = db.session.query(Logins.password).filter_by(username=uname).first()
+		if (not related_password == None) and password_info == related_password[0]:
+			return redirect('/')
+		return render_template('signin.html')
 	else:
 		return render_template('signin.html')
 
